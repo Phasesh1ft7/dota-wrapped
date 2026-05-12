3ff0d0255d3439830a50910286a6d9a3cd4f7e6a
Sounak Biswas <arrancarx4@icloud.com>
2026-05-12 23:19:25 +0530

    cleanup: remove wlYear debug log

--- app/wrapped/[accountId]/page.tsx
+++ app/wrapped/[accountId]/page.tsx
@@ -223,7 +223,6 @@ export default async function WrappedPage({ params, searchParams }: Props) {
   }
 
   const profile = profileResult.value;
-  console.log('[wlYear]', profile?.wlYear);
   const avatar = profile.player?.profile?.avatarfull ?? null;
   const name = profile.player?.profile?.personaname ?? null;
 
